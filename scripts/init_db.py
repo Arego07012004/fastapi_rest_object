a72a2219d9820e425e9ad02bcacadf5707bc68a8
Areg Harutyunyan <aregharutyunyan919@gmail.com>
2025-12-30 00:46:47 +0400

    Updated init_db.py

--- scripts/init_db.py
+++ scripts/init_db.py
@@ -1,33 +1,41 @@
 import psycopg2
+from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
 
 DB_NAME = "harward_university"
 DB_USER = "areg"
 DB_PASSWORD = "7125"
-DB_HOST = "127.0.0.1"
-DB_PORT = 5432
+DB_HOST = "localhost"
+DB_PORT = 5423
 
-try:
-    conn = psycopg2.connect(
-        dbname=DB_NAME,
-        user=DB_USER,
-        password=DB_PASSWORD,
-        host=DB_HOST,
-        port=DB_PORT
-    )
+conn = psycopg2.connect(
+    dbname=DB_NAME,
+    user=DB_USER,
+    password=DB_PASSWORD,
+    host=DB_HOST,
+    port=DB_PORT
+)
 
-    cur = conn.cursor()
-    print("Successfully connected to the database!")
+conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
+cur = conn.cursor()
 
-    cur.execute("""
-        SELECT table_name FROM information_schema.tables
-        WHERE table_schema='public';
-    """)
-    tables = cur.fetchall()
-    print("Tables in database:", tables)
+print("Successfully connected to the database!")
 
-    cur.close()
-    conn.close()
+#Check if role exists
+cur.execute(f"SELECT 1 FROM pg_roles WHERE rolname='{DB_USER}';")
+if not cur.fetchone():
+    cur.execute(f"CREATE ROLE {DB_USER} LOGIN PASSWORD '{DB_PASSWORD}';")
+    print(f"Role {DB_USER} created")
+else:
+    print(f"Role {DB_USER} already exists")
 
-except psycopg2.OperationalError as e:
-    print("Connection failed:", e)
+#Check if database exists
+cur.execute(f"SELECT 1 FROM pg_database WHERE datname='{DB_NAME}';")
+if not cur.fetchone():
+    cur.execute(f"CREATE DATABASE {DB_NAME} OWNER '{DB_USER}';")
+    print(f"Database {DB_NAME} created")
+else:
+    print(f"Database {DB_NAME} already exists")
 
+cur.close()
+conn.close()
+print("Database and user created successfully!")
