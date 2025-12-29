d94497014cf9ac59e61e71816c93b60426b3b304
Areg Harutyunyan <aregharutyunyan919@gmail.com>
2025-12-29 23:50:17 +0400

    Update init_db.py

--- scripts/init_db.py
+++ scripts/init_db.py
@@ -1,39 +1,33 @@
 import psycopg2
-from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
 
 DB_NAME = "harward_university"
 DB_USER = "areg"
 DB_PASSWORD = "7125"
-DB_HOST = "localhost"
+DB_HOST = "127.0.0.1"
 DB_PORT = 5432
 
-conn = psycopg2.connect(
-    dbname="postgres",
-    user="postgres",
-    password="postgres"
-    host=DB_HOST
-    port=DB_PORT
-)
+try:
+    conn = psycopg2.connect(
+        dbname=DB_NAME,
+        user=DB_USER,
+        password=DB_PASSWORD,
+        host=DB_HOST,
+        port=DB_PORT
+    )
 
-conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
-cur = conn.cursor()
+    cur = conn.cursor()
+    print("Successfully connected to the database!")
 
-# User
-cur.execute(f"""
-DO $$
-BEGIN
-    IF NOT EXISTS (SELECT FROM pg_roles WHERE rolname = '{DB_USER}') THEN
-       CREATE ROLE {DB_USER} LOGIN PASSWORD '{DB_PASSWORD}';
-    END IF;
-END
-$$;
-""")
+    cur.execute("""
+        SELECT table_name FROM information_schema.tables
+        WHERE table_schema='public';
+    """)
+    tables = cur.fetchall()
+    print("Tables in database:", tables)
 
-# Base
-cur.execute(f"""
-SELECT 'CREATE DATABASE {DB_NAME} OWNER {DB_USER}'
-WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = '{DB_NAME}');
-""")
+    cur.close()
+    conn.close()
 
-conn.close()
-print("Database initialized")
+except psycopg2.OperationalError as e:
+    print("Connection failed:", e)
+
